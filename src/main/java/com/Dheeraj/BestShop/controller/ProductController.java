72ca739ff62185b8eb345944b4320b02dfbc86b6
Dheeraj Kumar <kumardheeraj96@gmail.com>
2024-03-24 17:36:46 -0400

    added Editproduct.html and Delete product logic

--- src/main/java/com/Dheeraj/BestShop/controller/ProductController.java
+++ src/main/java/com/Dheeraj/BestShop/controller/ProductController.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -84,6 +81,91 @@ public class ProductController {
         product.setCreateAt(createAt);
         product.setImageFileName(storageFileName);
         service.save(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit")
+    public String showEditPage(
+            Model model, @RequestParam int id
+    ){
+        try{
+            Product product = service.findById(id).get();
+            model.addAttribute("product",product);
+            ProductDto productDto = new ProductDto();
+            productDto.setName(product.getName());
+            productDto.setBrand(product.getBrand());
+            productDto.setCategory(product.getCategory());
+            productDto.setPrice(product.getPrice());
+            productDto.setDescription(product.getDescription());
+
+            model.addAttribute("productDto",productDto);
+        }catch (Exception ex){
+            System.out.println("Exception: "+ex.getMessage());
+            return "redirect:/products";
+        }
+        return "products/EditProduct";
+    }
+
+    @PostMapping("/edit")
+    public String updateProduct(
+            Model model,
+            @RequestParam int id,
+            @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result
+    ){
+        try{
+            Product product = service.findById(id).get();
+            model.addAttribute("product",product);
+            if(result.hasErrors()){
+                return "products/EditProduct";
+            }
+            if(!productDto.getImageFile().isEmpty()){
+                String uploadDir ="public/images/";
+                Path oldImagePath = Paths.get(uploadDir + product.getImageFileName());
+                try{
+                    Files.delete(oldImagePath);
+                }catch (Exception ex){
+                    System.out.println("Exception: "+ex.getMessage());
+                }
+                MultipartFile image = productDto.getImageFile();
+                Date createAt = new Date();
+                String storageFileName = createAt.getTime()+"_"+image.getOriginalFilename();
+                try(InputStream inputStream = image.getInputStream()){
+                    Files.copy(inputStream,Paths.get(uploadDir + storageFileName),
+                            StandardCopyOption.REPLACE_EXISTING);
+                }
+                product.setImageFileName(storageFileName);
+            }
+            product.setName(productDto.getName());
+            product.setBrand(productDto.getBrand());
+            product.setCategory(productDto.getCategory());
+            product.setPrice(productDto.getPrice());
+
+            service.save(product);
+
+        }catch(Exception ex){
+            System.out.println("Exception: "+ex.getMessage());
+        }
+        return "redirect:/products";
+    }
+
+    @GetMapping("/delete")
+    public String deleteProduct(
+            @RequestParam int id
+    ){
+        try{
+            Product product=service.findById(id).get();
+
+            Path imagePath = Paths.get("public/images/"+product.getImageFileName());
+            try{
+                Files.delete(imagePath);
+            }catch (Exception ex){
+                System.out.println("Exception: "+ex.getMessage());
+            }
+            service.delete(product);
+        }catch(Exception ex){
+            System.out.println("Exception: "+ex.getMessage());
+        }
         return "redirect:/products";
     }
 }
--- src/main/java/com/Dheeraj/BestShop/service/ProductService.java
+++ src/main/java/com/Dheeraj/BestShop/service/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -21,5 +22,13 @@ public class ProductService {
 
     public void save(Product product) {
         repository.save(product);
+    }
+
+    public Optional<Product> findById(int id) {
+        return repository.findById(id);
+    }
+
+    public void delete(Product product) {
+        repository.delete(product);
     }
 }
